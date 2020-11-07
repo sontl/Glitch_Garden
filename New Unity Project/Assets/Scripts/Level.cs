00ff6ea6632a0075fa8336c1440ee5cd533506f3
Son Tran Lam <sontl@code.edu.vn>
2020-11-07 22:58:34 +0800

    Add Splash screen and Start screen

---
+++ New Unity Project/Assets/Scripts/Level.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] int timeToWait = 3;
+    private int currentSceneIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(LoadNextScene());
+        }
+        
+    }
+
+    IEnumerator LoadNextScene()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
